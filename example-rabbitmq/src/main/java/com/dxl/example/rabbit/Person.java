6dcd64df8f361c400d4ab8398f0ccf960b8d0880
duxl <869330465@qq.com>
2018-11-20 13:27:37 +0800

    commit by cmd

---
+++ example-rabbitmq/src/main/java/com/dxl/example/rabbit/Person.java
@@ -0,0 +1,17 @@
+package com.dxl.example.rabbit;
+
+import lombok.Data;
+
+/**
+ * @Program Person
+ * @Description
+ * @Author duxl
+ * @Create 2018/11/20 9:37
+ */
+@Data
+public class Person {
+
+    private String name;
+    private int age;
+
+}
